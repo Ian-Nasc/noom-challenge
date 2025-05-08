7209b8c486c1bf55132ab9d6d8abb4c297b00617
Ian Santos <iansantosnascimento@gmail.com>
2025-05-08 16:33:28 -0300

    Initial commit

---
+++ src/main/java/br/com/javaversiontest/JavaVersionTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.javaversiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaVersionTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaVersionTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/javaversiontest/JavaVersionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javaversiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaVersionTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
